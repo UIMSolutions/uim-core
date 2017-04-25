6bcc430f8297b8a25da85ac6dd26922654c876ec
UI Manufaktur <ui.manufaktur@gmail.com>
2017-04-25 10:12:41 +0200

    Update and rename package.d to source/package.d

---
+++ source/package.d
@@ -0,0 +1,9 @@
+module uim.core;
+
+public import std.stdio;
+public import std.string;
+public import std.array;
+public import std.algorithm;
+public import std.traits;
+
+public import uim.core.json;
