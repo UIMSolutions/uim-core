02f20b98c87d7b9674fa6f20d3259a48fb9df041
UI Manufaktur <ui.manufaktur@gmail.com>
2019-05-04 13:48:12 +0200

    More comments

--- source/uim/core/datatypes/boolean.d
+++ source/uim/core/datatypes/boolean.d
@@ -4,7 +4,30 @@ import uim.core;
 
 char[] BOOL(bool value) { return cast(char[])((value) ? `true`:`false`); }
 
+/**
+ * Toggle boolean value (from true to false, from false to true)
+ * Parameters:
+ * value = value to toggle
+ * 
+ * Example:
+ * auto b = true;
+ * auto a = toggle(b); // a = false
+*/
 bool toggle(bool value) { return !value; }
+
+/**
+ * Translates boolean value to defined values
+ * Parameters:
+ * value = incoming boolean
+ * ifTrue = result if value = true
+ * ifFalse = result if value = false
+ * 
+ * Example:
+ * auto b = true;
+ * auto a = translate(b, "red", "green"); // a = "red"
+ * or a = b.translate("red", "green"); // a = "red"
+ * 
+*/
 T translate(T)(bool value, T ifTrue, T ifFalse) { return (value) ? ifTrue : ifFalse; }
 bool translate(T)(T value, T ifTrue) { return (value == ifTrue); }
 
