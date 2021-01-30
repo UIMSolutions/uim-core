94890009fe31552a92cad9485ea35e2534e75433
ui-manufaktur <ui.manufaktur@gmail.com>
2021-01-30 16:58:28 +0100

    Update 02.2021

--- source/uim/core/mixins/function_.d
+++ source/uim/core/mixins/function_.d
@@ -1,5 +1,5 @@
 ﻿/***********************************************************************************************
-*	Copyright: © 2017-2020 UI Manufaktur UG
+*	Copyright: © 2017-2021 UI Manufaktur UG
 *	License: Subject to the terms of the MIT license, as written in the included LICENSE.txt file.
 *	Authors: UI Manufaktur Team
 *	Documentation [DE]: https://ui-manufaktur.com/docu/uim-core/mixins/function
