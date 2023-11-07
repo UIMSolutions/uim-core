cb7461175cca48670b62b78805c2e965c7492cff
UIMSolutions <ui.manufaktur@gmail.com>
2023-11-07 07:05:54 +0100

    Update License

--- source/uim/core/datatypes/datetime.d
+++ source/uim/core/datatypes/datetime.d
@@ -1,5 +1,5 @@
 ﻿/***********************************************************************************************************************
-*	Copyright: © 2017-2022 UI Manufaktur UG / 2022 Ozan Nurettin Süel (sicherheitsschmiede)                              *
+*	Copyright: © 2015-2023 Ozan Nurettin Süel (sicherheitsschmiede)                              *
 *	License: Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt]                                       *
 *	Authors: UI Manufaktur UG Team, Ozan Nurettin Süel (Sicherheitsschmiede)										                         * 
 ***********************************************************************************************************************/
