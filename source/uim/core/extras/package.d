ab7b1be84c6b54106e3e882431be2af94edd3982
UIMSolutions <ui.manufaktur@gmail.com>
2023-11-10 21:51:33 +0100

    Update

--- source/uim/core/extras/package.d
+++ source/uim/core/extras/package.d
@@ -1,3 +1,8 @@
+/***********************************************************************************************************************
+*	Copyright: © 2015-2023 Ozan Nurettin Süel (sicherheitsschmiede)                              *
+*	License: Licensed under Apache 2 [https://apache.org/licenses/LICENSE-2.0.txt]                                       *
+*	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)										                         * 
+***********************************************************************************************************************/
 module uim.core.extras;
 
 public {
